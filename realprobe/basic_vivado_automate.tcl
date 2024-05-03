04400f3192b8a18eff4906c15c983e7536786bde
Jiho Kim <61861533+jihoray@users.noreply.github.com>
2024-05-03 15:38:42 -0400

    Update basic_vivado_automate.tcl

--- realprobe/basic_vivado_automate.tcl
+++ realprobe/basic_vivado_automate.tcl
@@ -175,7 +175,7 @@ return 1
     puts $file "update_compile_order -fileset sources_1"
     puts $file "set_property top design_1_wrapper \[current_fileset\]"
     puts $file "update_compile_order -fileset sources_1"
-    puts $file "launch_runs impl_1 -to_step write_bitstream -jobs 4"
+    puts $file "launch_runs impl_1 -to_step write_bitstream -jobs 64"
     puts $file "wait_on_run impl_1"
     puts $file "close_project"
     puts $file "exit"
